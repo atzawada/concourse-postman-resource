fd849a1f5f9f7730700ff3d18b734c49e64878ce
atzaw <atzawada@gmail.com>
2019-07-14 15:20:22 -0400

    Add JSON Parsing

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,9 @@
 FROM node:8-alpine
 RUN npm install -g newman
+RUN npm install get-stdin
 
 COPY in.js      /in
 COPY out.js     /out
 COPY check.js   /check
 
 RUN chmod +x /in /out /check
-
-RUN apk add --no-cache
-ARG PYTHON_VERSION='3.7.0'
-
-
--- out.js
+++ out.js
@@ -1,3 +1,14 @@
 #! /usr/bin/env node
 
 console.log("hello");
+
+var stdin = process.stdin;
+var stdout = process.stdout;
+var params;
+
+const getStdin = require('get-stdin');
+
+(async() => {
+    params = JSON.parse(await getStdin());
+    console.log(params);
+})();
