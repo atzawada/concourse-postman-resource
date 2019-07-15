844152ce83e8ce6c2f2f4b62ddd158c278fe39cd
Zawada, Andrew <zawada1@nationwide.com>
2019-07-15 14:53:43 -0400

    Add dummy response for check script

---
+++ scripts/check.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+
+let response = {
+    "version": { "ref": "61cebf" },
+    "metadata": [
+      { "name": "commit", "value": "61cebf" },
+      { "name": "author", "value": "Hulk Hogan" }
+    ]
+  };
+
+console.log(JSON.stringify(response));
