7cb168300e620690b2eea2ed4b2c94ed9019ef6f
Zawada, Andrew <zawada1@nationwide.com>
2019-07-15 15:39:20 -0400

    Add test newman run

--- scripts/out.js
+++ scripts/out.js
@@ -5,6 +5,7 @@ var stdout = process.stdout;
 var params;
 
 const getStdin = require("get-stdin");
+const spawn = require("child_process");
 
 (async() => {
     params = JSON.parse(await getStdin());
@@ -12,6 +13,12 @@ const getStdin = require("get-stdin");
 })();
 
 process.stderr.write("hello");
+
+const newman = spawn("newman", ["run", "../test/test_data_file.json"]);
+
+newman.stdout.on('data', (data) => {
+    process.stderr.write(`stdout: ${data}`);
+});
 
 let response = {
     "version": { "ref": "61cebf" },
