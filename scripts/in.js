97d0839ef435d3486845c2c9e3f1e178178cce63
Zawada, Andrew <zawada1@nationwide.com>
2019-07-15 14:22:42 -0400

    Add dummy response to in script

---
+++ scripts/in.js
@@ -0,0 +1,9 @@
+let response = {
+    "version": { "ref": "61cebf" },
+    "metadata": [
+      { "name": "commit", "value": "61cebf" },
+      { "name": "author", "value": "Hulk Hogan" }
+    ]
+  };
+
+console.log(JSON.stringify(response));
