0b182ab7912eb7301152d79c721970b504f1309e
atzaw <atzawada@gmail.com>
2019-08-13 15:50:24 -0400

    More additional testing

--- scripts/out.js
+++ scripts/out.js
@@ -15,7 +15,7 @@ var newman_params = [];
 
 console.error(process.argv);
 
-spawnSync("ls", ["/opt/resource/out"], {stdio: ["ignore", process.stderr, process.stderr ] });
+spawnSync("ls", ["-alrt", "/opt/resource/out"], {stdio: ["ignore", process.stderr, process.stderr ] });
 
 // Read JSON input from stdin
 const rl = readline.createInterface({
@@ -147,7 +147,7 @@ function run() {
     console.error("HTML file has been copied to " + dump_location);
   }
 
-  spawnSync("ls", ["/opt/resource/out"], {stdio: ["ignore", process.stderr, process.stderr ] });
+  spawnSync("ls", ["-alrt", "/opt/resource/out"], {stdio: ["ignore", process.stderr, process.stderr ] });
 
   // Create response
   var date = new Date();
